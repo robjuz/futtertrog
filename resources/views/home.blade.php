f76d2210cf03b58c69fc959f91bf7e72afc1ff3c
robjuz <robjuz@gmail.com>
2019-01-05 21:47:48 +0100

    small css and layout fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,11 +25,11 @@
             </div>
 
             <div class="col-md-6 mb-3">
-                @if($todayMeals->count())
-                    <div class="card mb-3">
-                        <div class="card-header">{{ __('Your today order') }}</div>
+                <div class="card mb-3">
+                    <div class="card-header">{{ __('Your today order') }}</div>
 
-                        <div class="card-body">
+                    <div class="card-body">
+                        @if($todayMeals->count())
 
                             <table class="table">
                                 <thead>
@@ -47,68 +47,68 @@
                                 @endforeach
                                 </tbody>
                             </table>
+                        @else
+                            <div class="card bg-danger">
+
+                                <div class="card-body py-1">
+                                    {{ __('No orders for today') }}
+                                </div>
+                            </div>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-header">{{ __('Your upcoming orders') }}</div>
+
+                    <div class="card-body">
+                        <div class="card-deck flex-column">
+                            @if($futureMeals->count())
+                                <table class="table">
+                                    <thead>
+                                    <tr>
+                                        <th scope="col">{{ __('Date') }}</th>
+                                        <th scope="col">{{ __('Order') }}</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($futureMeals->groupBy(function($meal) { return $meal->date->format(__('futtertrog.d.m.Y')); }) as $date => $meals)
+
+                                        @foreach($meals as $meal)
+                                            <tr>
+                                                @if ($loop->iteration == 1)
+                                                    <th scope="row" rowspan="{{ $meals->count() }}">
+                                                        {{ $date }}
+                                                    </th>
+                                                @endif
+                                                <td>
+                                                    <strong>{{ $meal->title }}</strong>
+                                                    <div title="{{ $meal->description }}">
+                                                        {{ $meal->description }}
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    @endforeach
+                                    </tbody>
+                                </table>
+
+                                {{ $futureMeals->links() }}
                             @else
                                 <div class="card bg-danger">
 
                                     <div class="card-body py-1">
-                                        {{ __('No orders for today') }}
+                                        {{ __('No upcoming orders') }}
                                     </div>
+                                </div>
+                                <div class="card py-2 mt-2">
+                                    <a href="{{ route('meals.index') }}"
+                                       class="btn btn-primary">{{ __('Place order') }}</a>
                                 </div>
                             @endif
                         </div>
                     </div>
-
-                    <div class="card">
-                        <div class="card-header">{{ __('Your upcoming orders') }}</div>
-
-                        <div class="card-body">
-                            <div class="card-deck flex-column">
-                                @if($futureMeals->count())
-                                    <table class="table">
-                                        <thead>
-                                        <tr>
-                                            <th scope="col">{{ __('Date') }}</th>
-                                            <th scope="col">{{ __('Order') }}</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        @foreach($futureMeals->groupBy(function($meal) { return $meal->date->format(__('futtertrog.d.m.Y')); }) as $date => $meals)
-
-                                            @foreach($meals as $meal)
-                                                <tr>
-                                                    @if ($loop->iteration == 1)
-                                                        <th scope="row" rowspan="{{ $meals->count() }}">
-                                                            {{ $date }}
-                                                        </th>
-                                                    @endif
-                                                    <td>
-                                                        <strong>{{ $meal->title }}</strong>
-                                                        <div title="{{ $meal->description }}">
-                                                            {{ $meal->description }}
-                                                        </div>
-                                                    </td>
-                                                </tr>
-                                            @endforeach
-                                        @endforeach
-                                        </tbody>
-                                    </table>
-
-                                    {{ $futureMeals->links() }}
-                                @else
-                                    <div class="card bg-danger">
-
-                                        <div class="card-body py-1">
-                                            {{ __('No upcoming orders') }}
-                                        </div>
-                                    </div>
-                                    <div class="card py-2 mt-2">
-                                        <a href="{{ route('meals.index') }}"
-                                           class="btn btn-primary">{{ __('Place order') }}</a>
-                                    </div>
-                                @endif
-                            </div>
-                        </div>
-                    </div>
+                </div>
 
             </div>
 
