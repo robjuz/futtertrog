4e1d12cc9ab1825fa9e4e13627412187053059ef
robjuz <robjuz@gmail.com>
2019-01-14 21:34:21 +0100

    better week days translations

--- app/Notifications/NoOrder.php
+++ app/Notifications/NoOrder.php
@@ -3,9 +3,8 @@
 namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Notifications\Notification;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
 use Illuminate\Support\Carbon;
 
 class NoOrder extends Notification
@@ -30,7 +29,7 @@ class NoOrder extends Notification
     /**
      * Get the notification's delivery channels.
      *
-     * @param  mixed  $notifiable
+     * @param  mixed $notifiable
      * @return array
      */
     public function via($notifiable)
@@ -41,22 +40,23 @@ class NoOrder extends Notification
     /**
      * Get the mail representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param  mixed $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
     public function toMail($notifiable)
     {
-        $day = $this->date->isToday() ? __('today') : $this->date->localeDayOfWeek;
+        $day = $this->date->isToday() ? __('calendar.today') : __('calendar.' . $this->date->englishDayOfWeek);
+
         return (new MailMessage)
             ->subject(__('No order for', ['day' => $day]))
-                    ->line(__('This is a friendly reminder.'))
-                    ->line(__('You have ordered no food for!', ['day' => $day]));
+            ->line(__('This is a friendly reminder.'))
+            ->line(__('You have ordered no food for!', ['day' => $day]));
     }
 
     /**
      * Get the array representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param  mixed $notifiable
      * @return array
      */
     public function toArray($notifiable)
