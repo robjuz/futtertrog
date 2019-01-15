d3ef38edf917193ba5f081b055e7cb1c8e7f2862
Robert Juzak <robert.juzak@helmundwalter.de>
2019-01-15 11:22:27 +0100

    fixed settings migration

--- database/migrations/2019_01_06_140020_add_settings_to_users_table.php
+++ database/migrations/2019_01_06_140020_add_settings_to_users_table.php
@@ -14,7 +14,7 @@ class AddSettingsToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->json('settings');
+            $table->json('settings')->nullable();
         });
     }
 
