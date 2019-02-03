a2576120bd1922546abcc2403f873f095e825624
robjuz <robjuz@gmail.com>
2019-02-03 22:48:54 +0100

    disable register. only admin can create users

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
-Auth::routes();
+Auth::routes([
+    'register' => false
+]);
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/', 'HomeController')->name('home');
