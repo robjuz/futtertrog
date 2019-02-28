5594b0601a0494711c3bbaa35f86c51d85c0bbe6
robjuz <robjuz@gmail.com>
2019-02-28 21:47:45 +0100

    wip

--- resources/views/partials/order_history.blade.php
+++ resources/views/partials/order_history.blade.php
@@ -2,7 +2,9 @@
     <h2 class="card-header d-flex align-items-center">
         {{ __('Order history') }}
 
-        <a href="{{ route('meals.ical') }}" class="ml-auto btn btn-link">{{ __('Download as iCal') }}</a>
+        @if(request()->routeIs('home'))
+            <a href="{{ route('meals.ical') }}" class="ml-auto btn btn-link">{{ __('Download as iCal') }}</a>
+        @endif
     </h2>
 
     <div class="list-group list-group-flush">
