8471423a5a86c52dd4b9fe477f10d4237fa88ed8
robjuz <robjuz@gmail.com>
2019-03-08 07:04:27 +0000

    Apply fixes from StyleCI

--- resources/lang/en/futtertrog.php
+++ resources/lang/en/futtertrog.php
@@ -5,5 +5,5 @@ return [
     'status.open' => 'Open',
     'status.ordered' => 'Ordered',
     'meal_was_ordered' => 'This Menu was already ordered! Please delete all orders first.',
-    'available_meals' => '{0} There are <strong>none</strong> available meals for <span class="text-primary">:date</span>.|{1} There is <strong>:count</strong>available meal for <span class="text-primary">:date</span>.|[2, *] There are <strong>:count</strong> available meals for <span class="text-primary">:date</span>.'
+    'available_meals' => '{0} There are <strong>none</strong> available meals for <span class="text-primary">:date</span>.|{1} There is <strong>:count</strong>available meal for <span class="text-primary">:date</span>.|[2, *] There are <strong>:count</strong> available meals for <span class="text-primary">:date</span>.',
 ];
