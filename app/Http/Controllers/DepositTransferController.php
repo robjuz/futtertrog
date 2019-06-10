6e4f6dfacee65241769cbb2abe4505b533f3aa15
robjuz <robjuz@gmail.com>
2019-06-10 20:43:32 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/DepositTransferController.php
+++ app/Http/Controllers/DepositTransferController.php
@@ -2,14 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Deposit;
-use Illuminate\Support\Facades\DB;
 use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DepositTransferController extends Controller
 {
-
     public function show()
     {
         $users = User::all();
@@ -29,20 +28,20 @@ class DepositTransferController extends Controller
             'source' => 'required|exists:users,id',
             'target' => 'required|exists:users,id',
             'value' => 'required|numeric',
-            'comment' => 'nullable|string'
+            'comment' => 'nullable|string',
         ]);
 
-        DB::transaction(function() use ($request) {
+        DB::transaction(function () use ($request) {
             Deposit::create([
                 'user_id' => $request->source,
                 'value' => (-1) * $request->value,
-                'comment' => $request->comment
+                'comment' => $request->comment,
             ]);
-    
+
             Deposit::create([
                 'user_id' => $request->target,
                 'value' =>  $request->value,
-                'comment' => $request->comment
+                'comment' => $request->comment,
             ]);
         });
 
