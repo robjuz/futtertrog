9e10ceac0a141be192533b508bd3b0ddae2d5ef4
kasia <kasia.brodzka@interia.pl>
2019-08-21 21:00:22 +0200

    fancy svg pot :)

--- resources/views/meal/meal.blade.php
+++ resources/views/meal/meal.blade.php
@@ -1,66 +1,53 @@
-<div class="border-top border-bottom py-3 d-flex flex-column h-100">
-    <div class="actions d-flex mb-2">
-        @can('update', $meal)
-            <a href="{{ route('meals.edit', $meal) }}" class="btn btn-link text-info px-0">
-                {{ __('Edit') }}
-            </a>
-        @endcan
 
-        @can('delete', $meal)
-            <form action="{{ route('meals.destroy', $meal) }}" method="post" class="d-inline-block">
-                @method('delete')
-                @csrf
-                <button type="submit" class="btn btn-link text-danger">{{ __('Delete') }}</button>
-            </form>
-        @endcan
+<h4>
+    {{ $meal->title }}
+</h4>
 
-        @if($orderItem = $todayOrders->firstWhere('meal_id', $meal->id))
-            <form onsubmit="toggleOrder(event)" action="{{ route('order_items.destroy', $orderItem) }}" method="post"
-                  class="d-flex justify-content-end ml-auto">
-                @csrf
-                @method('delete')
-                <button type="submit" class="btn btn-outline-danger btn-submit">
-                    {{ __('Delete order') }}
-                    <span class="d-none spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-                </button>
-            </form>
-        @else
-           <form onsubmit="toggleOrder(event)" action="{{ route('order_items.store') }}" method="post"
-                  class="d-flex justify-content-end ml-auto">
-                @csrf
-                <input type="hidden" name="date" value="{{ $requestedDate->toDateString() }}"/>
-                <input type="hidden" name="user_id" value="{{ auth()->id() }}"/>
-                <input type="hidden" name="meal_id" value="{{ $meal->id }}"/>
+<small>{{ number_format($meal->price, 2, ',', '.') }} €</small>
 
-                <input type="number"
-                       class="form-control"
-                       name="quantity"
-                       min="1"
-                       pattern="\d*"
-                       value="1"
-                       style="width: 80px;"
-                >
-                <button type="submit" class="btn btn-outline-primary btn-submit">
-                    {{ __('Place order') }}
-                    <span class="d-none spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-                </button>
-            </form>
-        @endif
+@if (!(auth()->user()->settings[\App\User::SETTING_HIDE_ORDERING_MEAL_DESCRIPTION] ?? false))
+    <p>{{ $meal->description }}</p>
+@endif
 
-    </div>
+@can('update', $meal)
+    <a href="{{ route('meals.edit', $meal) }}">
+        {{ __('Edit') }}
+    </a>
+@endcan
 
-    <div class="d-sm-flex mb-2">
+@can('delete', $meal)
+    <form action="{{ route('meals.destroy', $meal) }}" method="post">
+        @method('delete')
+        @csrf
+        <button type="submit">{{ __('Delete') }}</button>
+    </form>
+@endcan
 
-        <h4 class="d-flex justify-content-between align-items-center flex-grow-1 mb-sm-0">
-            <div class="{{ $meal->getTitleClasses() }} mr-auto">
-                {{ $meal->title }}
-            </div>
-            <small class="text-nowrap">{{ number_format($meal->price, 2, ',', '.') }} €</small>
-        </h4>
+@if($orderItem = $todayOrders->firstWhere('meal_id', $meal->id))
+    <form onsubmit="toggleOrder(event)" action="{{ route('order_items.destroy', $orderItem) }}" method="post">
+        @csrf
+        @method('delete')
+        <button type="submit">
+            {{ __('Delete order') }}
+        </button>
+    </form>
+@else
+   <form onsubmit="toggleOrder(event)" action="{{ route('order_items.store') }}" method="post">
+        @csrf
+        <input type="hidden" name="date" value="{{ $requestedDate->toDateString() }}"/>
+        <input type="hidden" name="user_id" value="{{ auth()->id() }}"/>
+        <input type="hidden" name="meal_id" value="{{ $meal->id }}"/>
 
-    </div>
+        <input type="number"
+               name="quantity"
+               min="1"
+               pattern="\d*"
+               value="1"
+               style="width: 80px;"
+        >
+        <button type="submit">
+            {{ __('Place order') }}
+        </button>
+    </form>
+@endif
 
-    @if (!(auth()->user()->settings[\App\User::SETTING_HIDE_ORDERING_MEAL_DESCRIPTION] ?? false))
-        <p class="text-dark order-3 description">{{ $meal->description }}</p>
-    @endif
-</div>
--- resources/views/partials/order_history.blade.php
+++ resources/views/partials/order_history.blade.php
@@ -1,42 +1,70 @@
-<div class="card">
-    <h2 id="order-history" class="card-header d-flex align-items-center">
-        {{ __('Order history') }}
+<h2 id="order-history">
+    {{ __('Order history') }}
+</h2>
 
-        @if(request()->routeIs('home'))
-            <a href="{{ route('meals.ical') }}" class="ml-auto btn btn-link">{{ __('Download as iCal') }}</a>
-        @endif
-    </h2>
+@if(request()->routeIs('home') AND $orders->count())
+    <a href="{{ route('meals.ical') }}">{{ __('Download as iCal') }}</a>
+@endif
 
-    <ul class="list-group list-group-flush">
-        @forelse ($orders as $orderItem)
-            <li class="list-group-item {{ $loop->last ? ' border-bottom-0' : '' }}">
-                <header class="d-flex w-100 align-items-center mb-2">
-                    <h3 class="mb-0 h5 text-nowrap"> {{ $orderItem->quantity }} &times; {{ $orderItem->meal->title }}</h3>
-                    <small class="ml-auto" title="{{ $orderItem->created_at->format(__('futtertrog.datetime_format')) }}">{{ $orderItem->created_at->diffForHumans() }}</small>
-                </header>
+@if($orders)
+    <table>
+        <thead>
+            <tr>
+                <th><span class="sr-only">Anzahl</span></th>
+                <th>Menü</th>
+                <th>Bestellt am</th>
+                <th>Bestellt für</th>
+                <th>Preis</th>
+                @if(auth()->user()->is_admin AND !request()->routeIs('home'))
+                    <th>Aktionen</th>
+                @endif
+            </tr>
+        </thead>
+        @foreach ($orders as $orderItem)
+            <tr>
+                <td>
+                    {{ $orderItem->quantity }}
+                    <span aria-hidden="true">&times;</span>
+                </td>
 
-                <div class="d-flex w-100 justify-content-between align-items-center mb-2">
-                    <div>{{  $orderItem->order->date->format(__('futtertrog.date_format'))  }}</div>
-                    <div class="text-nowrap">{{ number_format($orderItem->meal->price, 2, ',','.') }} €</div>
-                </div>
+                <td>
+                     {{ $orderItem->meal->title }}
+                </td>
+
+                <td>
+                    <small title="{{ $orderItem->created_at->format(__('futtertrog.datetime_format')) }}">
+                        {{ $orderItem->created_at->diffForHumans() }}
+                    </small>
+                </td>
+
+                <td>
+                    <date>
+                        {{  $orderItem->order->date->format(__('futtertrog.date_format'))  }}
+                    </date>
+                </td>
+
+                <td>
+                    {{ number_format($orderItem->meal->price, 2, ',','.') }} €
+                </td>
 
                 @if(auth()->user()->is_admin AND !request()->routeIs('home'))
-                    <form action="{{ route('order_items.destroy', $orderItem) }}" method="post" class="text-right">
-                        @csrf
-                        @method('delete')
+                    <td>
+                        <form action="{{ route('order_items.destroy', $orderItem) }}" method="post">
+                            @csrf
+                            @method('delete')
 
-                        <button type="submit" class="btn btn-link btn-sm text-danger">
-                            {{ __('Delete') }}
-                        </button>
-                    </form>
+                            <button type="submit">
+                                {{ __('Delete') }}
+                            </button>
+                        </form>
+                    </td>
                 @endif
-            </li>
-        @empty
-            <div class="alert alert-warning m-0" role="alert">
-                <strong> {{ __('No results found') }}!</strong>
-            </div>
-        @endforelse
-    </ul>
+            </tr>
+        @endforeach
+    </table>
+@else
+    <p> {{ __('No results found') }}!</p>
+@endif
 
-    {{ $orders->links() }}
-</div>
+{{ $orders->links() }}
+
