2cc8f64d28e73eb61b051756900a69329b19b8a3
kasia <kasia.brodzka@interia.pl>
2019-08-26 21:11:04 +0200

    es6 class

--- public/js/app.js
+++ public/js/app.js
@@ -1,34 +1,41 @@
-function ExpandableMenu(nav) {
-    this.nav = nav;
+class ExpandableMenu {
+    constructor(nav) {
+        this.nav = nav;
 
-    this.nav.setAttribute('class', 'js');
-    this.createToggleButton();
+        this.nav.setAttribute('class', 'js');
+        this.createToggleButton();
+    }
+
+    createToggleButton() {
+        if(!!this.nav.getElementsByTagName('ul')[0]){
+            this.toggleButton = document.createElement('button');
+            this.toggleButton.insertAdjacentHTML('afterbegin',
+                '<span aria-hidden="true">&rarr;</span>' +
+                '<span class="sr-only">Menü</span>'
+            );
+            this.toggleButton.setAttribute('aria-haspopup', 'true');
+            this.toggleButton.setAttribute('aria-expanded', 'false');
+
+            this.nav.insertAdjacentElement('afterbegin', this.toggleButton);
+            this.toggleButton.addEventListener('click', this.onButtonClick.bind(this));
+        }
+    }
+
+    onButtonClick() {
+        if(this.isExpanded()) {
+            this.toggleButton.setAttribute('aria-expanded', 'false');
+            this.toggleButton.querySelector('[aria-hidden]').innerHTML = '&larr;';
+        } else {
+            this.toggleButton.setAttribute('aria-expanded', 'true');
+            this.toggleButton.querySelector('[aria-hidden]').innerHTML = '&rarr;';
+        }
+    }
+
+    isExpanded() {
+        return this.toggleButton.getAttribute('aria-expanded') === 'true';
+    }
+
 }
-
-ExpandableMenu.prototype.createToggleButton = function() {
-    if(!!this.nav.getElementsByTagName('ul')[0]){
-        this.toggleButton = document.createElement('button');
-        this.toggleButton.insertAdjacentHTML('afterbegin',
-            '<span aria-hidden="true">&rarr;</span>' +
-            '<span class="sr-only">Menü</span>'
-        );
-        this.toggleButton.setAttribute('aria-haspopup', 'true');
-        this.toggleButton.setAttribute('aria-expanded', 'false');
-
-        this.nav.insertAdjacentElement('afterbegin', this.toggleButton);
-        this.toggleButton.addEventListener('click', ExpandableMenu.prototype.onButtonClick.bind(this));
-    }
-};
-
-ExpandableMenu.prototype.onButtonClick = function() {
-    if(this.toggleButton.getAttribute('aria-expanded') === 'true') {
-        this.toggleButton.setAttribute('aria-expanded', 'false');
-        this.toggleButton.querySelectorAll('[aria-hidden]')[0].innerHTML = '&larr;';
-    } else {
-        this.toggleButton.setAttribute('aria-expanded', 'true');
-        this.toggleButton.querySelectorAll('[aria-hidden]')[0].innerHTML = '&rarr;';
-    }
-};
 
 /* Initialising instances */
 if(!!document.getElementById('main-navbar')) {
