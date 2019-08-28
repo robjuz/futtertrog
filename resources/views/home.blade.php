9dae82de114c736630a217d410ce4bf3b65eebf3
kasia <kasia.brodzka@interia.pl>
2019-08-28 22:15:26 +0200

    pot generator first try

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,80 +1,82 @@
 @extends('layouts.app')
 
+@push('main-classes')
+    dashboard
+@endpush
+
 @section('content')
-    <div class="dashboard">
-        <h1>{{ __('Dashboard') }}</h1>
+    <h1>{{ __('Dashboard') }}</h1>
 
-        <section>
-            <header>
-                <h2>{{ __('Your order for today') }}</h2>
-            </header>
+    <section>
+        <header>
+            <h2>{{ __('Your order for today') }}</h2>
+        </header>
 
-            @if($todayOrders->count())
+        @if($todayOrders->count())
+            <ul>
+                @foreach($todayOrders as $order)
+                    <li>
+                        <h3>
+                            {{ $order->quantity }} &times; {{ $order->meal->title }}
+                        </h3>
+                        @if (!(auth()->user()->settings[\App\User::SETTING_HIDE_DASHBOARD_MEAL_DESCRIPTION] ?? false))
+                            <p>{{ $order->meal->description }}</p>
+                        @endif
+                    </li>
+                @endforeach
+            </ul>
+        @else
+            <p>{{ __('No orders for today') }}</p>
+        @endif
+    </section>
+
+    <section>
+        <header>
+            <h2>{{ __('Your balance') }}</h2>
+        </header>
+
+        <p>{{ number_format($balance, 2, ',','.') }} €</p>
+    </section>
+
+    <section>
+        <header>
+            <h2 id="future-meals">{{ __('Your upcoming orders') }}</h2>
+        </header>
+
+            @if($futureOrders->count())
                 <ul>
-                    @foreach($todayOrders as $order)
+                    @foreach($futureOrders->groupBy(function($orderItem) { return $orderItem->order->date->format(__('futtertrog.date_format')); }) as $date => $orders)
                         <li>
-                            <h3>
-                                {{ $order->quantity }} &times; {{ $order->meal->title }}
-                            </h3>
-                            @if (!(auth()->user()->settings[\App\User::SETTING_HIDE_DASHBOARD_MEAL_DESCRIPTION] ?? false))
-                                <p>{{ $order->meal->description }}</p>
-                            @endif
+                            <h3>{{ $date }}</h3>
+
+                            @foreach( $orders as $order)
+                                <h4>
+                                    {{ $order->quantity }} &times; {{ $order->meal->title }}
+                                </h4>
+
+                                @if (!(auth()->user()->settings[\App\User::SETTING_HIDE_DASHBOARD_MEAL_DESCRIPTION] ?? false))
+                                    <p>{{ $order->meal->description }}</p>
+                                @endif
+                            @endforeach
                         </li>
                     @endforeach
                 </ul>
+
+                {{ $futureOrders->links() }}
+
             @else
-                <p>{{ __('No orders for today') }}</p>
+                <p>{{ __('No upcoming orders') }}</p>
+
+                <a href="{{ route('meals.index') }}">{{ __('Place order') }}</a>
             @endif
-        </section>
+    </section>
 
-        <section>
-            <header>
-                <h2>{{ __('Your balance') }}</h2>
-            </header>
-
-            <p>{{ number_format($balance, 2, ',','.') }} €</p>
-        </section>
-
-        <section>
-            <header>
-                <h2 id="future-meals">{{ __('Your upcoming orders') }}</h2>
-            </header>
-
-                @if($futureOrders->count())
-                    <ul>
-                        @foreach($futureOrders->groupBy(function($orderItem) { return $orderItem->order->date->format(__('futtertrog.date_format')); }) as $date => $orders)
-                            <li>
-                                <h3>{{ $date }}</h3>
-
-                                @foreach( $orders as $order)
-                                    <h4>
-                                        {{ $order->quantity }} &times; {{ $order->meal->title }}
-                                    </h4>
-
-                                    @if (!(auth()->user()->settings[\App\User::SETTING_HIDE_DASHBOARD_MEAL_DESCRIPTION] ?? false))
-                                        <p>{{ $order->meal->description }}</p>
-                                    @endif
-                                @endforeach
-                            </li>
-                        @endforeach
-                    </ul>
-
-                    {{ $futureOrders->links() }}
-
-                @else
-                    <p>{{ __('No upcoming orders') }}</p>
-
-                    <a href="{{ route('meals.index') }}">{{ __('Place order') }}</a>
-                @endif
-        </section>
-
-        <section>
-            @include('partials.deposit_history')
-        </section>
+    <section>
+        @include('partials.deposit_history')
+    </section>
 <?php /*
         <section id="deposit-history">
             @include('partials.order_history', ['orders' => $ordersHistory])
         </section>
 */?>
-    </div>
 @endsection
