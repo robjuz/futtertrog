7147a4b144fdf3f8725918435d38970b525d1e4b
robjuz <robjuz@gmail.com>
2020-01-02 22:20:28 +0100

    typo

--- resources/lang/de/futtertrog.php
+++ resources/lang/de/futtertrog.php
@@ -7,7 +7,7 @@ return [
     'date_format' => 'd.m.Y',
     'datetime_format' => 'd.m.Y H:i',
     'meal_was_ordered' => 'Dieses Menu wurde bereits bestellt! Bitte erst alle Bestellungen löschen.',
-    'portions_ordered' => '{1} 1 Portions bestellt|[2,*] :count Portionen bestellt',
+    'portions_ordered' => '{1} 1 Portion bestellt|[2,*] :count Portionen bestellt',
     'available_meals' => '{0} Es gibt <strong>keine</strong> Bestellmöglichkeiten für <span class="text-primary">:date</span>.|{1} Es gibt <strong>:count</strong> Bestellmöglichkeit für <span class="text-primary">:date</span>.|[2, *] Es gibt <strong>:count</strong> Bestellmöglichkeiten für <span class="text-primary">:date</span>.',
     'locale' => [
         'de' => 'Deutsch',
