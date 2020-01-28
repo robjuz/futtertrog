77526083b159b610737775de3cd33a0f95d7954d
robjuz <robjuz@gmail.com>
2020-01-28 06:51:05 +0100

    success message when saved user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -68,7 +68,7 @@ class UserController extends Controller
             return response()->json($user);
         }
 
-        return redirect()->route('users.edit', $user);
+        return redirect()->route('users.edit', $user)->with('success', __('Success'));
     }
 
     /**
@@ -133,7 +133,7 @@ class UserController extends Controller
 
         $user->fill($request->validated());
 
-        if ($request->has('password') && ! is_null($request->input('password'))) {
+        if ($request->has('password') && !is_null($request->input('password'))) {
             $user->password = Hash::make($request->input('password'));
         }
 
