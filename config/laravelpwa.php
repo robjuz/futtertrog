3d3580f3853ad41bfa22077e4e2d2d4a4fbf207a
robjuz <robjuz@gmail.com>
2020-07-03 04:01:27 +0000

    Apply fixes from StyleCI

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -13,35 +13,35 @@ return [
         'icons' => [
             '72x72' => [
                 'path' => '/images/icons/icon-72x72.jpeg',
-                'purpose' => 'any'
+                'purpose' => 'any',
             ],
             '96x96' => [
                 'path' => '/images/icons/icon-96x96.jpeg',
-                'purpose' => 'any'
+                'purpose' => 'any',
             ],
             '128x128' => [
                 'path' => '/images/icons/icon-128x128.jpeg',
-                'purpose' => 'any'
+                'purpose' => 'any',
             ],
             '144x144' => [
                 'path' => '/images/icons/icon-144x144.jpeg',
-                'purpose' => 'any'
+                'purpose' => 'any',
             ],
             '152x152' => [
                 'path' => '/images/icons/icon-152x152.jpeg',
-                'purpose' => 'any'
+                'purpose' => 'any',
             ],
             '192x192' => [
                 'path' => '/images/icons/icon-192x192.jpeg',
-                'purpose' => 'any'
+                'purpose' => 'any',
             ],
             '384x384' => [
                 'path' => '/images/icons/icon-384x384.jpeg',
-                'purpose' => 'any'
+                'purpose' => 'any',
             ],
             '512x512' => [
                 'path' => '/images/icons/icon-512x512.jpeg',
-                'purpose' => 'any'
+                'purpose' => 'any',
             ],
         ],
         'splash' => [
@@ -57,21 +57,21 @@ return [
             '2048x2732' => '/images/icons/splash-2048x2732.jpeg',
         ],
         'shortcuts' => [
-//            [
-//                'name' => 'Shortcut Link 1',
-//                'description' => 'Shortcut Link 1 Description',
-//                'url' => '/shortcutlink1',
-//                'icons' => [
-//                    "src" => "/images/icons/icon-72x72.jpeg",
-//                    "purpose" => "any"
-//                ]
-//            ],
-//            [
-//                'name' => 'Shortcut Link 2',
-//                'description' => 'Shortcut Link 2 Description',
-//                'url' => '/shortcutlink2'
-//            ]
+            //            [
+            //                'name' => 'Shortcut Link 1',
+            //                'description' => 'Shortcut Link 1 Description',
+            //                'url' => '/shortcutlink1',
+            //                'icons' => [
+            //                    "src" => "/images/icons/icon-72x72.jpeg",
+            //                    "purpose" => "any"
+            //                ]
+            //            ],
+            //            [
+            //                'name' => 'Shortcut Link 2',
+            //                'description' => 'Shortcut Link 2 Description',
+            //                'url' => '/shortcutlink2'
+            //            ]
         ],
-        'custom' => []
+        'custom' => [],
     ],
 ];
