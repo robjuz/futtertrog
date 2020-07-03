4dea898c62f0ca1df13d2ce43de7ce6c149b60fc
Robert Juzak <robjuz@gmail.com>
2020-07-03 05:59:22 +0200

    new pwa icon first try

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -1,37 +1,77 @@
 <?php
 
 return [
-    'name' => 'Futtertrog',
     'manifest' => [
         'name' => 'Futtertrog',
         'short_name' => 'Futtertrog',
-        'start_url' => '/',
+        'start_url' => '/login',
         'background_color' => '#f8fafc',
         'theme_color' => '#0177da',
         'display' => 'standalone',
         'orientation'=> 'any',
+        'status_bar' => 'black',
         'icons' => [
-            '72x72' => '/images/icons/icon-72x72.png',
-            '96x96' => '/images/icons/icon-96x96.png',
-            '128x128' => '/images/icons/icon-128x128.png',
-            '144x144' => '/images/icons/icon-144x144.png',
-            '152x152' => '/images/icons/icon-152x152.png',
-            '192x192' => '/images/icons/icon-192x192.png',
-            '384x384' => '/images/icons/icon-384x384.png',
-            '512x512' => '/images/icons/icon-512x512.png',
+            '72x72' => [
+                'path' => '/images/icons/icon-72x72.jpeg',
+                'purpose' => 'any'
+            ],
+            '96x96' => [
+                'path' => '/images/icons/icon-96x96.jpeg',
+                'purpose' => 'any'
+            ],
+            '128x128' => [
+                'path' => '/images/icons/icon-128x128.jpeg',
+                'purpose' => 'any'
+            ],
+            '144x144' => [
+                'path' => '/images/icons/icon-144x144.jpeg',
+                'purpose' => 'any'
+            ],
+            '152x152' => [
+                'path' => '/images/icons/icon-152x152.jpeg',
+                'purpose' => 'any'
+            ],
+            '192x192' => [
+                'path' => '/images/icons/icon-192x192.jpeg',
+                'purpose' => 'any'
+            ],
+            '384x384' => [
+                'path' => '/images/icons/icon-384x384.jpeg',
+                'purpose' => 'any'
+            ],
+            '512x512' => [
+                'path' => '/images/icons/icon-512x512.jpeg',
+                'purpose' => 'any'
+            ],
         ],
         'splash' => [
-            '640x1136' => '/images/icons/splash-640x1136.png',
-            '750x1334' => '/images/icons/splash-750x1334.png',
-            '828x1792' => '/images/icons/splash-828x1792.png',
-            '1125x2436' => '/images/icons/splash-1125x2436.png',
-            '1242x2208' => '/images/icons/splash-1242x2208.png',
-            '1242x2688' => '/images/icons/splash-1242x2688.png',
-            '1536x2048' => '/images/icons/splash-1536x2048.png',
-            '1668x2224' => '/images/icons/splash-1668x2224.png',
-            '1668x2388' => '/images/icons/splash-1668x2388.png',
-            '2048x2732' => '/images/icons/splash-2048x2732.png',
+            '640x1136' => '/images/icons/splash-640x1136.jpeg',
+            '750x1334' => '/images/icons/splash-750x1334.jpeg',
+            '828x1792' => '/images/icons/splash-828x1792.jpeg',
+            '1125x2436' => '/images/icons/splash-1125x2436.jpeg',
+            '1242x2208' => '/images/icons/splash-1242x2208.jpeg',
+            '1242x2688' => '/images/icons/splash-1242x2688.jpeg',
+            '1536x2048' => '/images/icons/splash-1536x2048.jpeg',
+            '1668x2224' => '/images/icons/splash-1668x2224.jpeg',
+            '1668x2388' => '/images/icons/splash-1668x2388.jpeg',
+            '2048x2732' => '/images/icons/splash-2048x2732.jpeg',
         ],
-        'custom' => [],
+        'shortcuts' => [
+//            [
+//                'name' => 'Shortcut Link 1',
+//                'description' => 'Shortcut Link 1 Description',
+//                'url' => '/shortcutlink1',
+//                'icons' => [
+//                    "src" => "/images/icons/icon-72x72.jpeg",
+//                    "purpose" => "any"
+//                ]
+//            ],
+//            [
+//                'name' => 'Shortcut Link 2',
+//                'description' => 'Shortcut Link 2 Description',
+//                'url' => '/shortcutlink2'
+//            ]
+        ],
+        'custom' => []
     ],
 ];
