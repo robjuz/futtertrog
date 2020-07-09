033e63e20a554a0745fa271f6d288e189534a718
robjuz <robjuz@gmail.com>
2020-07-09 21:52:58 +0000

    Apply fixes from StyleCI

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use App\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -28,7 +27,6 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Gate::after(function ($user) {
-
             if ($user->is_admin) {
                 return true;
             }
