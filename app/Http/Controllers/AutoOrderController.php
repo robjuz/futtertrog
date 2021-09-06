ed47b451b396c2fd86bf54c911a8f88eff56fcee
Robert Juzak <robert.juzak@helmundwalter.de>
2021-09-06 21:38:03 +0200

    fixed auto order

--- app/Http/Controllers/AutoOrderController.php
+++ app/Http/Controllers/AutoOrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\MealProviders\AbstractMealProvider;
 use App\Repositories\OrdersRepository;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -20,8 +21,10 @@ class AutoOrderController extends Controller
     ) {
         $providerOrders = $ordersRepository->getByProvider($request);
 
-        foreach ($providerOrders as $provider => $orders) {
-            if ($provider = app($provider)->supportsAutoOrder()) {
+        foreach ($providerOrders as $providerName => $orders) {
+            /** @var AbstractMealProvider $provider */
+            $provider = app($providerName);
+            if ($provider->supportsAutoOrder()) {
                 $provider->placeOrder($orders);
             }
         }
--- app/Repositories/OrdersRepository.php
+++ app/Repositories/OrdersRepository.php
@@ -7,6 +7,7 @@ use App\User;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class OrdersRepository
 {
@@ -81,6 +82,8 @@ class OrdersRepository
 
     public function getByProvider(Request $request)
     {
-        return $this->get($request)->groupBy('provider');
+        return $this->get($request)->groupBy(function(Order $order) {
+            return $order->getAttributes()['provider'];
+        });
     }
 }
