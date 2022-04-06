21143c3d860384cfbfcf4c6ae7c2ae710e59341d
Sascha Creutzburg <sascha.creutzburg@helmundwalter.de>
2022-04-06 10:51:14 +0000

    Resolve "Dashboard CSS refactor"

--- resources/views/partials/deposit_history.blade.php
+++ resources/views/partials/deposit_history.blade.php
@@ -1,6 +1,21 @@
 <h2>{{ __('Deposit history') }} </h2>
 
+
+
+
 @if( count($deposits->items()) > 0)
+
+<ul class="deposit-list">
+    @foreach ($deposits as $deposit)
+        <li class="deposit-list__item">
+            <h3>@money($deposit->value)</h3>
+            <span>{{ $deposit->comment }}</span>
+            <small>{{ $deposit->created_at->format(__('futtertrog.date_format')) }}</small>
+        </li>
+    @endforeach
+</ul>
+
+    {{--
     <table>
         <thead>
             <tr>
@@ -20,6 +35,10 @@
             </tr>
         @endforeach
     </table>
+
+    --}}
+
+
 @else
     <p> {{ __('No results found') }}!</p>
 @endif
